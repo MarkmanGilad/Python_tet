f10fb5842a6228713630024ae07b51e5fd32ceba
Gilad <gilad@markman.co.il>
2025-09-08 11:07:31 +0300

    if

---
+++ 5_If_else.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+
+if num1 > num2:
+    print (num1, ">", num2)
+elif num1 < num2:
+    print (num2, ">", num1)
+else:
+    print (num1, "=", num2)
+    
+
+if num1 > 100:
+    print ("num1 is bigger that 100")
+    
+    
+if num1 < 100:
+    print ("Less that 100")
+else:
+    print("equal or bigger than 100")
